5bc328379398a5e1aea0ed54969ca5623225a161
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-05-03 20:55:38 +0200

    Update style for mobile

--- scripts/buttonStyle.js
+++ scripts/buttonStyle.js
@@ -1,12 +1,4 @@
 let buttons = document.getElementsByTagName('button')
 let buttonDiv = document.getElementById('buttonDiv')
-// document.onload(setStyle())
 
-// function setStyle(){
-    // buttons.forEach(element => {
-    //     element.style.height = window.innerHeight/ 2
-    // });
-    buttonDiv.style.height = screen.height - 150 + "px"
-    // console.log(buttonDiv)
-    console.log(screen.height, buttonDiv.style.height, window.innerHeight)
-// }
+buttonDiv.style.height = screen.height - 150 + "px"
--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,49 @@ const port = `8080`
 const baseURL = `http://192.168.0.67:${port}`
 let responseText = document.getElementById('responseText')
 
-function startStop() {
+async function startStop(button) {
     fetch(`${baseURL}/api/v1/startstop`).then(v => v.json()).then((response) => {
         responseText.innerHTML = response
         console.log(response)
     })
+    button.classList.toggle('activatedButton')
 }
 
-function next(){
+async function next(button){
     fetch(`${baseURL}/api/v1/forwards`).then(v => v.json()).then((response) => {
         responseText.innerHTML = response
         console.log(response)
     })
+
+    button.classList.toggle('activatedButton')
+    await sleep(600);
+    button.classList.toggle('activatedButton')
 }
 
-function previous(){
+async function previous(button){
     fetch(`${baseURL}/api/v1/backwards`).then(v => v.json()).then((response) => {
         responseText.innerHTML = response
         console.log(response)
     })
+
+    button.classList.toggle('activatedButton')
+    await sleep(600);
+    button.classList.toggle('activatedButton')
 }
 
-function reset(){
+async function reset(button){
     fetch(`${baseURL}/api/v1/reset`).then(v => v.json()).then((response) => {
         responseText.innerHTML = response
         console.log(response)
     })
+
+    button.classList.toggle('activatedButton')
+    await sleep(600);
+    button.classList.toggle('activatedButton')
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
