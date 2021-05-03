f62b234824e399f1b61b7e699e6ef5e8ba69155c
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-05-03 16:04:37 +0200

    Update response to json

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,29 @@ const baseURL = `http://192.168.0.67:${port}`
 let responseText = document.getElementById('responseText')
 
 function startStop() {
-    fetch(`${baseURL}/api/v1/startstop`).then((response) => {
-        responseText.innerHTML = response.body
+    fetch(`${baseURL}/api/v1/startstop`).then(v => v.json()).then((response) => {
+        responseText.innerHTML = response
         console.log(response)
     })
 }
 
 function next(){
-    fetch(`${baseURL}/api/v1/forwards`).then((response) => {
+    fetch(`${baseURL}/api/v1/forwards`).then(v => v.json()).then((response) => {
+        responseText.innerHTML = response
         console.log(response)
     })
 }
 
 function previous(){
-    fetch(`${baseURL}/api/v1/backwards`).then((response) => {
+    fetch(`${baseURL}/api/v1/backwards`).then(v => v.json()).then((response) => {
+        responseText.innerHTML = response
         console.log(response)
     })
 }
 
 function reset(){
-    fetch(`${baseURL}/api/v1/reset`).then((response) => {
+    fetch(`${baseURL}/api/v1/reset`).then(v => v.json()).then((response) => {
+        responseText.innerHTML = response
         console.log(response)
     })
 }
